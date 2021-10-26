97e47a891818695405e51e5577c9b26154231176
Max Grim <ik@maxgrim.nl>
2021-10-26 12:07:20 +0200

    Added Adafruit_CircuitPython_Register

--- ports/esp32s2/boards/ooo_2021/mpconfigboard.mk
+++ ports/esp32s2/boards/ooo_2021/mpconfigboard.mk
@@ -35,4 +35,5 @@ FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_ImageLoad
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_IRRemote
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_PCF8563
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Register
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Requests
